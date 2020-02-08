4f38aa6a4a3fa392405dee09e46938136e600eee
Life is Now <keylasan28@yahoo.com>
2020-02-08 13:30:48 -0500

    got quotes API working

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+//Omar's code starts here//
+
 //declaring a global variable of userInput that we can change and use from anywhere in the script.
 var userInput = "";
 //declaring default value of testURL to be our URL based on titles.
@@ -39,3 +41,61 @@ function runAjax() {
     console.log(response)
     });
 }
+
+//Omar's code ends here//
+
+
+
+
+
+
+
+
+
+//Keyla's code starts here//
+
+//researching working APIs
+
+            //quotes by author or image quotes//----> not working
+  //var queryUrl = "https://quote-garden.herokuapp.com/quotes/author/:j.k.rowling"
+  //var queryURL = "http://quotes.rest/quote/image/search.json?api_key=c9kZNAbwJv_8tdUeQinJMQeF?author=<JeanineCummins>"
+  //var queryURL = "http://quotes.rest/quote/image/search.json?author=JeanineCumminsapi_key=c9kZNAbwJv_8tdUeQinJMQeF"
+  //var queryURL = "http://quotes.rest/quote/image/search.json?author=JeanineCummins&api_key=c9kZNAbwJv_8tdUeQinJMQeF"
+
+              //books by author---->doesn't work
+  //var queryURL= "http://quotes.rest/quote/search.json?author=tolkien&api_key=c9kZNAbwJv_8tdUeQinJMQeF"
+ //var queryURL ="http://openlibrary.org/search.json?author=tolkien"
+
+          //with images----->doesn't work
+//var queryURL = "http://quotes.rest/quote/image/search.json"
+
+        //works well
+//this one is lame ----> var queryURL = "http://quotes.rest/qod.json"
+
+         //works perfectly for random authors
+var queryURL = "http://quotes.rest/quote/random.json?api_key=c9kZNAbwJv_8tdUeQinJMQeF"
+
+//we bought a key, but aren't using it-----> api_key=c9kZNAbwJv_8tdUeQinJMQeF
+
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+
+//What to do with the quotes {code here}
+//Put in Div
+
+//console.log(response);
+
+$("#quote-author").text(response.contents.author);
+$("#quote-text").text(response.contents.quote);
+
+
+});
+
+
+
+
+
+//Keyla's code ends here//
