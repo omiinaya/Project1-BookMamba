b074d2048257557b4cb6d9aecf37da5dca5bc5af
Omar Minaya <omiinaya@gmail.com>
2020-02-11 05:00:07 -0800

    created a function that filters our results so only high ratings are displayed.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,54 @@
 var responseTest = "";
 var userInput = "";
-var queryURL = "http://quotes.rest/quote/random.json?api_key=c9kZNAbwJv_8tdUeQinJMQeF"
+var newArr = [];
+
+function loadPage() {
+  defaultDracula()
+  quotesAjax()
+  enterKey()
+}
+
+function enterKey(){
+  var enterText = document.getElementById("search-bar");
+  enterText.addEventListener("keyup", function(event) {
+    if (event.keyCode===13) {
+      event.preventDefault();
+      runAjax();
+    }
+  });
+}
+
+function ratingFilter() {
+  for (var i = 0; i < responseTest.items.length; i++)
+  if (responseTest.items[i].volumeInfo.averageRating >= 4) {
+    newArr.push(responseTest.items[i]);
+  }
+  console.log(newArr);
+}
+
+function nextBook() {
+  passData()
+}
+
+//declaring default value of testURL to be our URL based on titles.
+function runAjax() {
+  newArr = [];
+  userInput = $("#search-bar").val();
+  var testURL = "https://www.googleapis.com/books/v1/volumes?q="+userInput;
+  $.ajax({
+  url: testURL,
+  method: "GET"
+  }).then(function(response) {
+  console.log(response)
+  //assigning global variable responseTest the value of repsonse so we can use response outside of this function.
+  responseTest = response;
+  ratingFilter()
+  passData()
+  });
+}
 
 function quotesAjax(){
+  var queryURL = "http://quotes.rest/quote/random.json?api_key=c9kZNAbwJv_8tdUeQinJMQeF"
   $.ajax({
   url: queryURL,
   method: "GET"
@@ -13,25 +59,6 @@ function quotesAjax(){
   });
 }
 
-//declaring default value of testURL to be our URL based on titles.
-function runAjax() {
-  userInput = $("#search-bar").val();
-  var testURL = "https://www.googleapis.com/books/v1/volumes?q="+userInput;
-  $.ajax({
-  url: testURL,
-  method: "GET"
-  }).then(function(response) {
-  console.log(response)
-  //assigning global variable responseTest the value of repsonse so we can use response outside of this function.
-  responseTest = response;
-  passData()
-  });
-}
-
-function nextBook() {
-  passData()
-}
-
 function defaultDracula() {
 var testURL = "https://www.googleapis.com/books/v1/volumes?q=Dracula";
 $.ajax({
@@ -40,48 +67,18 @@ method: "GET"
 }).then(function(response) {
 console.log(response)
 responseTest = response;
+ratingFilter()
 passData()    
 });
 }
 
-function loadPage() {
-  defaultDracula()
-  quotesAjax()
-  enterKey()
-}
-
-//triggering enter key on search-bar button
-function enterKey(){
-var enterText = document.getElementById("search-bar");
-enterText.addEventListener("keyup", function(event)
-{
-  if (event.keyCode===13)
-  {
-    event.preventDefault();
-    runAjax();
-  }
-});
-}
-//Keyla's code ends here//
-
 function passData() {
-  var a = [Math.floor(Math.random()*responseTest.items.length)]
-  if (responseTest.items[a].volumeInfo.averageRating==5) {
-    $("#bookTitle").html(responseTest.items[a].volumeInfo.title);
-    $("#authorSpan").text(responseTest.items[a].volumeInfo.authors);
-    $("#publishedDate").text(responseTest.items[a].volumeInfo.publishedDate);
-    $("#rating").text(responseTest.items[a].volumeInfo.averageRating);
-    $("#bookCover").attr("src",responseTest.items[a].volumeInfo.imageLinks.thumbnail);
-    $("#descriptionText").text(responseTest.items[a].volumeInfo.description);
-    console.log(responseTest.items[a].volumeInfo.averageRating);
-  }
-  else if (responseTest.items[a].volumeInfo.averageRating>=4) {
-    $("#bookTitle").html(responseTest.items[a].volumeInfo.title);
-    $("#authorSpan").text(responseTest.items[a].volumeInfo.authors);
-    $("#publishedDate").text(responseTest.items[a].volumeInfo.publishedDate);
-    $("#rating").text(responseTest.items[a].volumeInfo.averageRating);
-    $("#bookCover").attr("src",responseTest.items[a].volumeInfo.imageLinks.thumbnail);
-    $("#descriptionText").text(responseTest.items[a].volumeInfo.description);
-  }     
+  var a = [Math.floor(Math.random()*newArr.length)]
+    $("#bookTitle").html(newArr[a].volumeInfo.title);
+    $("#authorSpan").text(newArr[a].volumeInfo.authors);
+    $("#publishedDate").text(newArr[a].volumeInfo.publishedDate);
+    $("#rating").text(newArr[a].volumeInfo.averageRating);
+    $("#bookCover").attr("src",newArr[a].volumeInfo.imageLinks.thumbnail);
+    $("#descriptionText").text(newArr[a].volumeInfo.description);
+    console.log(newArr[a].volumeInfo.averageRating);
 }
-    
