1aa5538425543c94a966c1d00d0d8bd8d8d65cbd
Omar Minaya <omiinaya@gmail.com>
2020-02-14 02:00:06 -0800

    playing around with URL parameters.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -193,3 +193,37 @@ function buyNow() {
   window.open("http://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="+titleName+authorName);
   });
 }
+//experimental
+//
+// Parse the URL parameter
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+// Give the parameter a variable name
+var dynamicContent = getParameterByName('dc');
+
+$(document).ready(function() {
+
+// Check if the URL parameter is apples
+if (dynamicContent) {
+  alert(dynamicContent);
+} 
+// Check if the URL parameter is oranges
+else if (dynamicContent == 'oranges') {
+  $('#oranges').show();
+} 
+// Check if the URL parameter is bananas
+else if (dynamicContent == 'bananas') {
+  $('#bananas').show();
+} 
+// Check if the URL parmeter is empty or not defined, display default content
+else {
+  $('#default-content').show();
+}
+});
